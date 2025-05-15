0401e2f4a640e58441894feb5948c3bc3b743307
Kazuya Watanabe <kazuya.watanabe@workram.com>
2025-05-15 14:50:05 +0900

    2025-05-15

--- rc/windows/powershell/Microsoft.PowerShell_profile.ps1
+++ rc/windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,6 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 $PSDefaultParameterValues['*:Encoding'] = 'utf8'
 
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-
 Import-Module PSReadLine
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -EditMode Vi
@@ -16,8 +13,6 @@ Set-PSReadLineKeyHandler -Key Ctrl+b -Function BackwardChar
 Set-PSReadLineKeyHandler -Key Ctrl+f -Function ForwardChar
 Set-PSReadLineKeyHandler -Key Ctrl+p -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key Ctrl+n -Function HistorySearchForward
-Set-PSReadLineKeyHandler -Key Ctrl+s -Function ForwardSearchHistory
-Set-PSReadLineKeyHandler -Key Ctrl+r -Function ReverseSearchHistory
 Set-PSReadLineKeyHandler -Key Ctrl+h -Function BackwardDeleteChar
 Set-PSReadLineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 Set-PSReadLineKeyHandler -Key Ctrl+k -Function KillLine
@@ -27,10 +22,45 @@ Set-PSReadLineKeyHandler -Key Ctrl+[ -Function ViCommandMode
 
 Import-Module posh-git
 
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'
+Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r'
+
 Remove-Item alias:curl
 Remove-Item alias:diff -Force
 Remove-Item alias:tee -Force
 Remove-Item alias:wget
+
+if (Get-Command -Name bat.exe -ErrorAction SilentlyContinue) {
+    Remove-Item alias:cat
+    Set-Alias cat bat.exe
+
+    $env:FZF_DEFAULT_OPTS = '--style full --height 40% --layout=reverse --preview "bat --color=always --style=plain --line-range=:500 {}"'
+} else {
+    $env:FZF_DEFAULT_OPTS = '--style full --height 40% --layout=reverse'
+}
+
+if (Get-Command diff.exe -ErrorAction SilentlyContinue) {
+    function diff {
+        &diff.exe -u @args
+    }
+}
+
+if (Get-Command fd.exe -ErrorAction SilentlyContinue) {
+    function fd {
+        &fd.exe --follow --hidden @args
+    }
+
+    $env:FZF_DEFAULT_COMMAND = 'fd --hidden --follow --type f'
+}
+
+if (Get-Command git.exe -ErrorAction SilentlyContinue) {
+    Set-Alias g git.exe
+
+    function gf {
+        &git.exe flow @args
+    }
+}
 
 if (Get-Command lsd.exe -ErrorAction SilentlyContinue) {
   Remove-Item alias:ls
@@ -47,9 +77,14 @@ if (Get-Command lsd.exe -ErrorAction SilentlyContinue) {
   Set-Alias ll ls
 }
 
-if (Get-Command -Name bat.exe -ErrorAction SilentlyContinue) {
-  Remove-Item alias:cat
-  Set-Alias cat bat.exe
+if (Get-Command less.exe -ErrorAction SilentlyContinue) {
+    $env:PAGER = 'less'
+    $env:LESS = '-iFJMRX'
+}
+
+if (Get-Command vim.exe -ErrorAction SilentlyContinue) {
+    $env:VISUAL = 'vim'
+    $env:EDITOR = 'vim'
 }
 
 function which() {
