217123715b7e753894467cfd5b05121bffb09c2e
Kazuya Watanabe <kazuya.watanabe@workram.com>
2025-05-23 09:06:39 +0900

    i3設定のモード表示改善とランチャーにsteam追加、ripgreprc削除、vimrcにfzfのRgコマンド追加、zshエイリアス整理と拡充、PowerShellプロファイルのfzf設定統一
    - i3 configのモード表示を複数行に分割し見やすく修正
    - i3ランチャーモードにsteam起動キーを追加
    - ripgreprcファイルを削除（設定を環境変数等に移行）
    - vimrcにfzfのRgコマンドを追加しripgrep検索を強化
    - zshのエイリアスを整理しbat、fd、rgなどの設定を統一・拡充
    - PowerShellプロファイルでfzfのデフォルトコマンドとオプションをrgベースに統一し、batやfdの関数を追加
    - 全体的に検索ツールをfdからrgへ移行し一貫性を向上

--- rc/windows/powershell/Microsoft.PowerShell_profile.ps1
+++ rc/windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -30,41 +30,13 @@ $env:EDITOR = 'vim'
 $env:VISUAL = 'vim'
 $env:PAGER = 'less'
 $env:LESS = '-iFJMRX'
-$env:FZF_DEFAULT_COMMAND = 'fd --hidden --follow --type f'
+$env:FZF_DEFAULT_COMMAND = 'rg --files --hidden --follow'
+$env:FZF_DEFAULT_OPTS = '--style full --height 40% --layout=reverse --preview "bat --color=always --style=plain --line-range=:500 {}"'
 
 Remove-Item alias:curl
 Remove-Item alias:diff -Force
 Remove-Item alias:tee -Force
 Remove-Item alias:wget
-
-if (Get-Command -Name bat.exe -ErrorAction SilentlyContinue) {
-    Remove-Item alias:cat
-    Set-Alias cat bat.exe
-
-    $env:FZF_DEFAULT_OPTS = '--style full --height 40% --layout=reverse --preview "bat --color=always --style=plain --line-range=:500 {}"'
-} else {
-    $env:FZF_DEFAULT_OPTS = '--style full --height 40% --layout=reverse'
-}
-
-function fd { &fd.exe --follow --hidden @args }
-function rg { &rg.exe --follow --hidden @args }
-
-Set-Alias g git.exe
-function gC { &git.exe commit @args }
-function gP { &git.exe push @args }
-function gR { &git.exe rebase @args }
-function gS { &git.exe stash @args }
-function ga { &git.exe add @args }
-function gb { &git.exe branch @args }
-function gc { &git.exe checkout @args }
-function gd { &git.exe diff @args }
-function gf { &git.exe flow @args }
-function gl { &git.exe log @args }
-function gm { &git.exe merge @args }
-function gp { &git.exe pull @args }
-function gr { &git.exe reset @args }
-function gs { &git.exe status @args }
-function gt { &git.exe tag @args }
 
 if (Get-Command lsd.exe -ErrorAction SilentlyContinue) {
   Remove-Item alias:ls
@@ -73,8 +45,14 @@ if (Get-Command lsd.exe -ErrorAction SilentlyContinue) {
   function la { &lsd.exe --group-directories-first -A @args }
   function ll { &lsd.exe --group-directories-first -Al @args }
 } else {
+  Set-Alias la ls
   Set-Alias ll ls
 }
+
+function bat { &bat.exe --style=plain --color=always @args }
+function fd { &fd.exe --follow --hidden @args }
+Set-Alias g git.exe
+function rg { &rg.exe --follow --hidden @args }
 
 function which() {
   param(
