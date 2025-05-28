e4a25db43dfe0c9e71859557cbaf72bca10e9e2f
Kazuya Watanabe <kazuya.watanabe@workram.com>
2025-05-28 15:33:56 +0900

    Windows環境のScoopインストールスクリプトをリスト形式からコマンド形式に変更し、PowerShellプロファイルの微調整を実施
    - Scoopのインストールリスト(rc/windows-scoop.list)を削除し、新たにコマンド実行形式のスクリプト(rc/windows-scoop.cmd)を追加
    - Scoopでインストールするパッケージを明示的にコマンドで記述し、バケット追加も含む
    - PowerShellプロファイル(rc/powershell/Microsoft.PowerShell_profile.ps1)でfzfのデフォルトコマンドにファイルタイプ指定(--type f)を追加し、関数定義のスペースを修正

---
+++ rc/windows-scoop.cmd
@@ -0,0 +1,25 @@
+scoop install bat
+scoop install delta
+scoop install fd
+scoop install fnm
+scoop install fzf
+scoop install gh
+scoop install git
+scoop install jq
+scoop install less
+scoop install lf
+scoop install lsd
+scoop install python
+scoop install ripgrep
+scoop install starship
+scoop install watchman
+scoop install wget
+scoop install zenhan
+scoop install zoxide
+
+scoop bucket add extras
+scoop install lazygit
+scoop install universal-ctags
+
+scoop bucket add versions
+scoop install vim-nightly
